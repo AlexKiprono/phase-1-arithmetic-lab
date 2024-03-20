7558043dff259a428bc5cad6eb62f1aa73469f10
AlexKiprono <lexiekiprono@gmail.com>
2024-03-20 19:19:17 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-
 // Write your code here
+
+//multiply
+const num1 = 62;
+const num2 = 1;
+const multiply = num1*num2;
+
+//random
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+//mod
+
+const num3 =10
+const num4 =6
+const mod = num3 % num4;
+
+//max
+
+const numbers = [10, 5, 'abc', 20, 15];
+const filteredNumbers = numbers.filter(num => typeof num === 'number');
+
+const max = filteredNumbers.length > 0 ? Math.max(...filteredNumbers) : NaN;
